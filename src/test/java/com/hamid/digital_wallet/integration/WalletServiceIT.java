7e01408ec21f998eb840b99b761bdf2fdcc2ec09
Hamid Sarfaraz <hamidsarfraz8@gmail.com>
2025-12-28 19:38:10 +0530

    added integration test for wallet service

---
+++ src/test/java/com/hamid/digital_wallet/integration/WalletServiceIT.java
@@ -0,0 +1,90 @@
+package com.hamid.digital_wallet.integration;
+
+import com.hamid.digital_wallet.entity.User;
+import com.hamid.digital_wallet.entity.Wallet;
+import com.hamid.digital_wallet.repository.UserRepository;
+import com.hamid.digital_wallet.service.WalletService;
+import com.hamid.digital_wallet.repository.WalletRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cglib.core.Local;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class WalletServiceIT {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    WalletService walletService;
+
+    @Autowired
+    WalletRepository walletRepository;
+
+    @Test
+    void shouldTransferMoneyCorrectly() {
+
+        // ---- USER 1 ----
+        User user1 = new User();
+        user1.setName("UserOne");
+        user1.setEmail("user1@test.com");
+        user1.setPasswordHash("12345");
+//        user1.setPhone("12345");
+        user1.setStatus("ACTIVE");
+        user1.setCreatedAt(LocalDateTime.now());
+        user1.setUpdatedAt(LocalDateTime.now());
+        user1.setWallet(null);
+
+        user1 = userRepository.saveAndFlush(user1);
+
+        Wallet fromWallet = new Wallet();
+        fromWallet.setUser(user1);
+        fromWallet.setBalance(new BigDecimal("1000"));
+//        fromWallet.getCreatedAt(LocalDateTime.now());
+        walletRepository.saveAndFlush(fromWallet);
+
+        // ---- USER 2 ----
+        User user2 = new User();
+        user2.setName("UserTwo");
+        user2.setEmail("user2@test.com");
+        user2.setPasswordHash("54321");
+        user2.setStatus("ACTIVE");
+        user2.setCreatedAt(LocalDateTime.now());
+        user2.setUpdatedAt(LocalDateTime.now());
+        user2.setWallet(null);
+
+        user2 = userRepository.saveAndFlush(user2);
+
+        Wallet toWallet = new Wallet();
+        toWallet.setUser(user2);
+        toWallet.setBalance(BigDecimal.ZERO);
+        walletRepository.saveAndFlush(toWallet);
+
+        // ---- TRANSFER ----
+        walletService.transfer(
+                fromWallet.getId(),
+                toWallet.getId(),
+                new BigDecimal("300"),
+                "REF_001"
+        );
+
+        Wallet updatedFrom = walletRepository.findById(fromWallet.getId()).orElseThrow();
+        Wallet updatedTo = walletRepository.findById(toWallet.getId()).orElseThrow();
+
+        System.out.println("Updated FROM wallet :" + updatedFrom.getUser().getWallet() + " and value is : " + updatedFrom.getBalance());
+        System.out.println("Updated TO wallet :" + updatedTo.getUser().getWallet() + " and value is : " + updatedTo.getBalance());
+//        System.out.println("Updated to value : " + updatedTo.getBalance());
+
+        assertEquals(new BigDecimal("700"), updatedFrom.getBalance());
+        assertEquals(new BigDecimal("300"), updatedTo.getBalance());
+    }
+
+}
